2941ebc8a24ddaf0f0f7c696f54f543c4d1500ee
Robert Such <rs228@njit.edu>
2021-07-28 15:49:56 -0400

    Completed Task 6. No screenshot as output has not changed. Updated Readme.

--- flask_container/application/__init__.py
+++ flask_container/application/__init__.py
@@ -1,16 +1,28 @@
-"""Initialize app."""
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_redis import FlaskRedis
+
+
+# Globally accessible libraries
+db = SQLAlchemy()
+r = FlaskRedis()
 
 
 def create_app():
-    """Construct the core flask."""
+    """Initialize the core application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object("config.Config")
-    app.config["RECAPTCHA_PUBLIC_KEY"] = "iubhiukfgjbkhfvgkdfm"
-    app.config["RECAPTCHA_PARAMETERS"] = {"size": "100%"}
+    app.config.from_object('config.Config')
+
+    # Initialize Plugins
+    db.init_app(app)
+    r.init_app(app)
 
     with app.app_context():
-        # Import parts of our flask
+        # Include our Routes
         from . import routes
+
+        # Register Blueprints
+        # app.register_blueprint(auth.auth_bp)
+        # app.register_blueprint(admin.admin_bp)
 
         return app
