570f18d53954db6fd770cdf82e4df7a0dcf205e2
Robert Such <rs228@njit.edu>
2021-07-29 03:07:00 -0400

    Completed Task 7. Updated Readme.  Added screenshot for output of main page.  Product info is not populated due to lack of API Key for Best Buy.

--- flask_container/application/__init__.py
+++ flask_container/application/__init__.py
@@ -1,28 +1,32 @@
+"""Initialize Flask app."""
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_redis import FlaskRedis
+from flask_assets import Environment
+from ddtrace import patch_all
 
 
-# Globally accessible libraries
-db = SQLAlchemy()
-r = FlaskRedis()
+patch_all()
 
 
-def create_app():
-    """Initialize the core application."""
+def init_app():
+    """Create Flask application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
-
-    # Initialize Plugins
-    db.init_app(app)
-    r.init_app(app)
+    app.config.from_object("config.Config")
+    assets = Environment()
+    assets.init_app(app)
 
     with app.app_context():
-        # Include our Routes
-        from . import routes
+        # Import parts of our application
+        from .assets import compile_static_assets
+        from .home import home
+        from .products import products
+        from .profile import profile
 
         # Register Blueprints
-        # app.register_blueprint(auth.auth_bp)
-        # app.register_blueprint(admin.admin_bp)
+        app.register_blueprint(profile.profile_bp)
+        app.register_blueprint(home.home_bp)
+        app.register_blueprint(products.product_bp)
+
+        # Compile static assets
+        compile_static_assets(assets)
 
         return app
--- flask_container/wsgi.py
+++ flask_container/wsgi.py
@@ -1,7 +1,7 @@
 """Application entry point."""
-from application import create_app
+from application import init_app
 
-app = create_app()
+app = init_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
