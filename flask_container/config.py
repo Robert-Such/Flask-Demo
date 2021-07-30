8816158fc80ab867a81fe7125be5c56de7cddb1d
Robert Such <rs228@njit.edu>
2021-07-30 05:31:42 -0400

    Completed Task 11. Added screenshot for output of main page.

--- flask_container/config.py
+++ flask_container/config.py
@@ -1,30 +1,37 @@
-"""Flask app configuration."""
+"""App configuration."""
 from os import environ, path
 
+import redis
 from dotenv import load_dotenv
 
 basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+load_dotenv(path.join(basedir, ".env"))
 
 
 class Config:
-    """Set Flask configuration from environment variables."""
+    """Set Flask configuration variables from .env file."""
 
-    FLASK_APP = environ.get('FLASK_APP')
-    FLASK_ENV = environ.get('FLASK_ENV')
-    SECRET_KEY = environ.get('SECRET_KEY')
+    # General Config
+    FLASK_APP = environ.get("FLASK_APP")
+    FLASK_ENV = environ.get("FLASK_ENV")
+    SECRET_KEY = environ.get("SECRET_KEY")
 
-    # Flask-SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_ECHO = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Flask-Session
+    REDIS_URI = "redis://redis:6379"
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.from_url('redis://redis:6379')
 
     # Flask-Assets
-    LESS_BIN = environ.get('LESS_BIN')
-    ASSETS_DEBUG = environ.get('ASSETS_DEBUG')
-    LESS_RUN_IN_DEBUG = environ.get('LESS_RUN_IN_DEBUG')
+    LESS_BIN = environ.get("LESS_BIN")
+    ASSETS_DEBUG = False
+    LESS_RUN_IN_DEBUG = False
 
     # Static Assets
-    STATIC_FOLDER = 'static'
-    TEMPLATES_FOLDER = 'templates'
-    COMPRESSOR_DEBUG = environ.get('COMPRESSOR_DEBUG')
+    STATIC_FOLDER = "static"
+    TEMPLATES_FOLDER = "templates"
+    COMPRESSOR_DEBUG = True
+
+    # Flask-SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
