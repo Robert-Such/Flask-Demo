86eb1cbb72165121128d413823d45cf449b6c959
Robert Such <rs228@njit.edu>
2021-07-30 02:57:32 -0400

    Completed Task 10. Added screenshot for output of main page.

--- flask_container/application/__init__.py
+++ flask_container/application/__init__.py
@@ -1,23 +1,38 @@
-"""Initialize Flask app."""
+"""Initialize app."""
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 # from ddtrace import patch_all
 
 
-db = SQLAlchemy()
 # patch_all()
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 def create_app():
-    """Construct the core application."""
+    """Construct the core app object."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object("config.Config")
+    app.config.from_object('config.Config')
 
+    # Initialize Plugins
     db.init_app(app)
+    login_manager.init_app(app)
 
     with app.app_context():
-        from . import routes  # Import routes
+        from . import routes
+        from . import auth
+        from .assets import compile_static_assets
 
-        db.create_all()  # Create database tables for our data models
+        # Register Blueprints
+        app.register_blueprint(routes.main_bp)
+        app.register_blueprint(auth.auth_bp)
+
+        # Create Database Models
+        db.create_all()
+
+        # Compile static assets
+        if app.config['FLASK_ENV'] == 'development':
+            compile_static_assets(app)
 
         return app
--- flask_container/config.py
+++ flask_container/config.py
@@ -1,5 +1,6 @@
-"""Flask configuration variables."""
+"""Flask app configuration."""
 from os import environ, path
+
 from dotenv import load_dotenv
 
 basedir = path.abspath(path.dirname(__file__))
@@ -7,14 +8,23 @@ load_dotenv(path.join(basedir, '.env'))
 
 
 class Config:
-    """Set Flask configuration from .env file."""
+    """Set Flask configuration from environment variables."""
 
-    # General Config
-    SECRET_KEY = environ.get('SECRET_KEY')
     FLASK_APP = environ.get('FLASK_APP')
     FLASK_ENV = environ.get('FLASK_ENV')
+    SECRET_KEY = environ.get('SECRET_KEY')
 
-   # Database
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    # Flask-SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Flask-Assets
+    LESS_BIN = environ.get('LESS_BIN')
+    ASSETS_DEBUG = environ.get('ASSETS_DEBUG')
+    LESS_RUN_IN_DEBUG = environ.get('LESS_RUN_IN_DEBUG')
+
+    # Static Assets
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+    COMPRESSOR_DEBUG = environ.get('COMPRESSOR_DEBUG')
